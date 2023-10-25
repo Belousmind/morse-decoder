bed8a57317c57d976943bbb942c44ad0daaf6923
Darya Belousova <belousmind@gmail.com>
2023-10-25 16:41:10 +0200

    complete task

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ const MORSE_TABLE = {
 function decode(expr) {
     const splitWords = expr.split('**********');
     let wordsCount = [];
-    let words = []
+    let words = []; 
     splitWords.forEach((element, index) => {
         const letters = element.split('');
         const counts = letters.length / 10;
@@ -51,10 +51,27 @@ function decode(expr) {
         }
         words.push(letter);
     });
-    console.log(words);
+    let sentence = [];
+    words.forEach(i => {
+        const morse = [];
+        for (n in i) {
+            let j = 0;
+            letters = [];
+            while(j < i[n].length) {
+                if (i[n].slice(j, j+2) == 10) {
+                    letters.push(`.`)
+                } else if (i[n].slice(j, j+2) == 11) {
+                    letters.push(`-`)
+                }
+                j+=2;
+            }
+            morse.push(MORSE_TABLE[letters.join('')])
+        }
+        sentence.push(morse.join(''))
+    })
+    console.log(sentence.join(' '));
+    return sentence.join(' ')
 }
-
-decode("0000101110000011111100000010110000111010**********00000000110000111111**********00101111100000101110000011111100001111100000101110000000101100000011110000001111000000101000000011100000111110")
 
 module.exports = {
     decode
