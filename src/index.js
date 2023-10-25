258bd99773e8876252fed05f1bb16f9a5f71a620
Darya Belousova <belousmind@gmail.com>
2023-10-25 02:37:32 +0200

    complete half of task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,23 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const splitWords = expr.split('**********');
+    let wordsCount = [];
+    let words = []
+    splitWords.forEach((element, index) => {
+        const letters = element.split('');
+        const counts = letters.length / 10;
+        wordsCount.push(counts);
+        const letter = [];
+        for (let i = 0; i < wordsCount[index]; i++) {
+            letter.push(element.slice((i)+'0', ((i+1)+'0')));
+        }
+        words.push(letter);
+    });
+    console.log(words);
 }
+
+decode("0000101110000011111100000010110000111010**********00000000110000111111**********00101111100000101110000011111100001111100000101110000000101100000011110000001111000000101000000011100000111110")
 
 module.exports = {
     decode
